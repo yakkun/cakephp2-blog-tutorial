0a0d7f7e982c557ca23a8d89e134a19a737b3c0e
YAKKUN Yasuyoshi IWAICHI <yakkun@yakkun.dev>
2021-01-13 01:01:56 +0900

    Add an endpoint: /posts/add

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -1,6 +1,7 @@
 <?php
 class PostsController extends AppController {
 	public $helpers = array('Html', 'Form');
+	public $components = array('Flash');
 
 	public function index() {
 		$this->set('posts', $this->Post->find('all'));
@@ -16,5 +17,16 @@ class PostsController extends AppController {
 				throw new NotFoundException(__('Invalid post'));
 		}
 		$this->set('post', $post);
+	}
+
+	public function add() {
+		if ($this->request->is('post')) {
+				$this->Post->create();
+				if ($this->Post->save($this->request->data)) {
+						$this->Flash->success(__('Your post has been saved.'));
+						return $this->redirect(array('action' => 'index'));
+				}
+				$this->Flash->error(__('Unable to add your post.'));
+		}
 	}
 }
