37eab92c1ec8d26a1a4f58fb26a6cf7c129c4d13
YAKKUN Yasuyoshi IWAICHI <yakkun@yakkun.dev>
2021-01-13 00:58:25 +0900

    Add an endpoint: /posts/view/{id}

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -5,4 +5,16 @@ class PostsController extends AppController {
 	public function index() {
 		$this->set('posts', $this->Post->find('all'));
 	}
+
+	public function view($id = null) {
+		if (!$id) {
+				throw new NotFoundException(__('Invalid post'));
+		}
+
+		$post = $this->Post->findById($id);
+		if (!$post) {
+				throw new NotFoundException(__('Invalid post'));
+		}
+		$this->set('post', $post);
+	}
 }
