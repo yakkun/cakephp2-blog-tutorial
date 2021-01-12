bdddfeff7f3291010aca352a144efaea23f61e8a
YAKKUN Yasuyoshi IWAICHI <yakkun@yakkun.dev>
2021-01-13 00:15:59 +0900

    Update Security.salt to suppress notice

--- app/Config/core.php
+++ app/Config/core.php
@@ -233,7 +233,7 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'oUubWwvniR2G0FgaC9miDYhG93b0qyJfIxfs2guV');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
